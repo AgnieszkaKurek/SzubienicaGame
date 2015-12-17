3e69ca11f7c993a198bbfafba9a56edd12803e0c
AgnieszkaKurek <agniechakurek@gmail.com>
2015-12-17 15:11:22 +0100

    Changing the language of record

--- szubienica.js
+++ szubienica.js
@@ -1,110 +1,110 @@
-﻿var haslo = new Array (20);
+﻿var password= new Array (20);
 var random = Math.floor((Math.random() * 20) + 1);
 switch (random) 
 
-haslo[0] = "Bez pracy nie ma kołaczy"; 
-haslo[1]= "Bogatemu to i byk się ocieli";
-haslo[2] = "Co dwie głowy to nie jedna";
-haslo[3]= "Co nie siłą to rozumem"; 
-haslo[4] = "Co za dużo to nie zdrowo";
-haslo[5]= "Co zaszkodziło to nauczyło";
-haslo[6] = "Czyja rola tego wola"; 
-haslo[7]= "Czuć się jak ryba w wodzie"; 
-haslo[8] = "Dla chcącego nic trudnego"; 
-haslo[9]= "Dobry zwyczaj nie pożyczaj"; 
-haslo[10] = "Drzewa umierają stojąc"; 
-haslo[11]= "Dziecii i ryby głosu nie mają"; 
-haslo[12] = "I kwaśne jabłko robak toczy"; 
-haslo[13]= "Im dalej w las tym więcej dzrew"; 
-haslo[14] = "Idzie luty podkuj buty"; 
-haslo[15]= "Jak cię widzą tak cię piszą";
-haslo[16] = "Jak krew w piach";
-haslo[17] = "Jak kamień w wodę";
-haslo[18] = "Jaki ojciec, taki syn"; 
-haslo[19] = "Jak trwoga to do boga"; 
+password[0] = "Bez pracy nie ma kołaczy"; 
+password[1]= "Bogatemu to i byk się ocieli";
+password[2] = "Co dwie głowy to nie jedna";
+password[3]= "Co nie siłą to rozumem"; 
+password[4] = "Co za dużo to nie zdrowo";
+password[5]= "Co zaszkodziło to nauczyło";
+password[6] = "Czyja rola tego wola"; 
+password[7]= "Czuć się jak ryba w wodzie"; 
+password[8] = "Dla chcącego nic trudnego"; 
+password[9]= "Dobry zwyczaj nie pożyczaj"; 
+password[10] = "Drzewa umierają stojąc"; 
+password[11]= "Dziecii i ryby głosu nie mają"; 
+password[12] = "I kwaśne jabłko robak toczy"; 
+password[13]= "Im dalej w las tym więcej dzrew"; 
+password[14] = "Idzie luty podkuj buty"; 
+password[15]= "Jak cię widzą tak cię piszą";
+password[16] = "Jak krew w piach";
+password[17] = "Jak kamień w wodę";
+password[18] = "Jaki ojciec, taki syn"; 
+password[19] = "Jak trwoga to do boga"; 
 
 
-haslo = haslo.toUpperCase();
-var dlugosc = haslo.length;
-var ile_skuch = 0;
+password = password.toUpperCase();
+var length = password.length;
+var how_many_mistakes = 0;
 var yes = new Audio("yes.wav");
 var no = new Audio("no.wav");
 
 
 
-function wypisz_haslo()
+function type_password()
 {
-    document.getElementById("plansza").innerHTML = haslo;
+    document.getElementById("board").innerHTML = password;
 }
 window.onload = start;
-var litery = new Array(35);
-litery[0] = "A";
-litery[1] = "Ą";
-litery[2] = "B";
-litery[3] = "C";
-litery[4] = "Ć";
-litery[5] = "D";
-litery[6] = "E";
-litery[7] = "Ę";
-litery[8] = "F";
-litery[9] = "G";
-litery[10] = "H";
-litery[11] = "I";
-litery[12] = "J";
-litery[13] = "K";
-litery[14] = "L";
-litery[15] = "Ł";
-litery[16] = "M";
-litery[17] = "N";
-litery[18] = "Ń";
-litery[19] = "O";
-litery[20] = "Ó";
-litery[21] = "P";
-litery[22] = "Q";
-litery[23] = "R";
-litery[24] = "S";
-litery[25] = "Ś";
-litery[26] = "T";
-litery[27] = "U";
-litery[28] = "V";
-litery[29] = "W";
-litery[30] = "X";
-litery[31] = "Y";
-litery[32] = "Z";
-litery[33] = "Ż";
-litery[34] = "Ż";
+var letters = new Array(35);
+letters[0] = "A";
+letters[1] = "Ą";
+letters[2] = "B";
+letters[3] = "C";
+letters[4] = "Ć";
+letters[5] = "D";
+letters[6] = "E";
+letters[7] = "Ę";
+letters[8] = "F";
+letters[9] = "G";
+letters[10] = "H";
+letters[11] = "I";
+letters[12] = "J";
+letters[13] = "K";
+letters[14] = "L";
+letters[15] = "Ł";
+letters[16] = "M";
+letters[17] = "N";
+letters[18] = "Ń";
+letters[19] = "O";
+letters[20] = "Ó";
+letters[21] = "P";
+letters[22] = "Q";
+letters[23] = "R";
+letters[24] = "S";
+letters[25] = "Ś";
+letters[26] = "T";
+letters[27] = "U";
+letters[28] = "V";
+letters[29] = "W";
+letters[30] = "X";
+letters[31] = "Y";
+letters[32] = "Z";
+letters[33] = "Ż";
+letters[34] = "Ż";
 
 
 function start() {
-    var tresc_diva = "";
+    var contents_div = "";
     for (i = 0; i <= 34; i++) {
         var element = "lit" + i;
-        tresc_diva = tresc_diva + '<div class="litera" onclick="sprawdz(' + i + ')" id="' + element + '">' + litery[i] + '</div>';
-        if ((i + 1) % 7 == 0) tresc_diva = tresc_diva + ' <div style="clear:both;"></div>';
+        contents_div = contents_div + '<div class="letters" onclick="check(' + i + ')" id="' + element + '">' + letters[i] + '</div>';
+        if ((i + 1) % 7 == 0) contents_div = contents_div + ' <div style="clear:both;"></div>';
     }
-    document.getElementById("alfabet").innerHTML = tresc_diva;
-    wypisz_haslo();
+    document.getElementById("alphabet").innerHTML = contents_div;
+    type_password();
 }
-String.prototype.ustawZnak = function (miejsce, znak) {
-    if (miejsce > this.length - 1) return this.toString();
-    else return this.substr(0, miejsce) + znak + this.substr(miejsce + 1);
+String.prototype.setMark = function (space, mark) {
+    if (space > this.length - 1) return this.toString();
+    else return this.substr(0, space) + mark + this.substr(space + 1);
 }
-function sprawdz(nr) {
-    var trafiona = false;
-    for (i = 0; i < dlugosc; i++) {
-        if (haslo.charAt(i) == litery[nr]) {
-            haslo1 = haslo1.ustawZnak(i, litery[nr]);
-            trafiona = true;
+function check(nr) {
+    var hit = false;
+    for (i = 0; i < length; i++) {
+        if (password.charAt(i) == letters[nr]) {
+            password1 = password1.setMark(i, letters[nr]);
+            hit = true;
         }
     }
-    if (trafiona == true) {
+    if (hit == true) {
         yes.play();
         var element = "lit" + nr;
         document.getElementById(element).style.background = "#003300";
         document.getElementById(element).style.color = "#00C000";
         document.getElementById(element).style.border = "3px solid #00C000";
         document.getElementById(element).style.cursor = "default";
-        wypisz_haslo();
+        type_password();
     }
     else {
         no.play();
@@ -115,19 +115,19 @@ function sprawdz(nr) {
         document.getElementById(element).style.cursor = "default";
         document.getElementById(element).setAttribute("onclick", ";");
 
-        //skucha
-        ile_skuch++;
-        var obraz = "img/s" + ile_skuch + ".jpg";
-        document.getElementById("szubienica").innerHTML = '<img src = "' + obraz + '"alt=""/>';
+        //mistake
+        how_many_mistake++;
+        var Image = "img/s" + how_many_mistake + ".jpg";
+        document.getElementById("gibbet").innerHTML = '<img src = "' + Image + '"alt=""/>';
 
         }
 
-    //wygrana 
-    if (haslo == haslo1)
-        document.getElementById("alfabet").innerHTML = "Tak jest! Podano prawidłowe hasło:" + haslo + '<br/><br/><span class="reset" onclick="location.reload() ">Jeszcze raz?</span>';
+    //win
+    if (password == password1)
+        document.getElementById("alphabet").innerHTML = "Tak jest! Podano prawidłowe hasło:" + password + '<br/><br/><span class="reset" onclick="location.reload() ">Jeszcze raz?</span>';
    
 
-    //przegrana
-    if (ile_skuch>=9)
-        document.getElementById("alfabet").innerHTML = "Nie udało się!!! Prawidłowe hasło:" + haslo + '<br/><br/><span class="reset" onclick="location.reload() ">Jeszcze raz?</span>';
+    //loss
+    if (how_many_mistake>=9)
+        document.getElementById("alphabet").innerHTML = "Nie udało się!!! Prawidłowe hasło:" + password + '<br/><br/><span class="reset" onclick="location.reload() ">Jeszcze raz?</span>';
 }
